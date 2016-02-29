1abd309ec736dd2c00429a2b9483c58654f8b29a
danielmarbach <daniel.marbach@openplace.net>
2016-02-29 21:39:34 +0100

    Initial stuff

---
+++ webinar/Script.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using NUnit.Framework;
+
+namespace AsyncDolls
+{
+    [TestFixture]
+    public class AsyncScript
+    {
+    }
+}
